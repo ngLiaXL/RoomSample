83093a140856040b8f1b3408b9fd8c5a5043f0d8
ngliaxl <ngliaxl@gmail.com>
2018-02-28 16:58:49 +0800

    opz

--- app/src/main/java/com/ldroid/room/MainActivity.java
+++ app/src/main/java/com/ldroid/room/MainActivity.java
@@ -1,13 +1,32 @@
 package com.ldroid.room;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.ldroid.room.persistence.User;
+import com.ldroid.room.persistence.UsersDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * val机制的sqlite
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        new Thread(){
+            @Override
+            public void run() {
+                final List<User> all = UsersDatabase.getInstance(MainActivity.this).userDao()
+                        .getAll();
+            }
+        }.start();
+
     }
+
 }
